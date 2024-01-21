18c9af0859935c5759557d09e388b3870d37655b
Genrikh Antipov <genrikhantipov@gmail.com>
2024-01-21 18:42:24 +0300

    feat(task-01): complete task 01

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,3 +1,10 @@
 //connect modules
 const fs = require('node:fs');
 const path = require('node:path'); 
+
+//script body
+const filePath = path.join(__dirname, 'text.txt')
+
+const readFile = fs.createReadStream(filePath, 'utf-8');
+
+readFile.on('data', data => console.log(data));
