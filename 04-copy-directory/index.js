5cfdf6b38ede7d8e0ee8d68b1fe9938e5a28eb2c
Genrikh Antipov <genrikhantipov@gmail.com>
2024-01-22 16:50:20 +0300

    fix(task-04): add funcionality to rewrite newdir after changes

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,3 +1,4 @@
+// i think it should be less complicated, but it was my first idea and i dont have time for another 
 //modules
 const fs = require('fs');
 const path = require('path');
@@ -12,14 +13,30 @@ copyDir();
 //function body
 function copyDir() {
   // create new folder 
-  fs.mkdir(newFolderPath, { recursive: true }, (err) => {
+  fs.mkdir(newFolderPath, (err) => {
     if (err) {
-      console.log('Ошибка при создании папки');
-      return;
+      console.log('Папка уже существует, включаю перезапись');
+      fs.rm(newFolderPath, { recursive: true }, (err) => {  //delete old folder
+        if (err) {
+          console.log('Ошибка при удалении папки');
+          return
+        }
+        fs.mkdir(newFolderPath, { recursive: true }, (err) => {  //create new folder again  
+          if (err) {
+            console.log('Ошибка при создании новой папки');
+            return;
+          }
+          copyEachFile();
+        });
+      });
     } 
-  });
-
+  });  
   //read directory
+  copyEachFile();
+}
+
+//function for copy files
+function copyEachFile() {
   fs.readdir(folderPath, (err, files) => {
     if (err) {
       console.log('Ошибка при чтении папки');
