735c5c46cf832d2ae3b5eec829fb1370f4c81cc0
Genrikh Antipov <genrikhantipov@gmail.com>
2024-01-22 21:51:44 +0300

    feat(task-05): complete task 05

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,26 @@
+//modules
+const fs = require('fs').promises;  
+const path = require('path');
+
+//path const
+const bundlePath = path.join(__dirname, 'project-dist/bundle.css');
+const stylesPath = path.join(__dirname, 'styles');
+
+//function body
+async function packStyles() {
+  fs.writeFile(bundlePath, '', 'utf-8');
+  
+  const files = await fs.readdir(stylesPath);
+
+  files.forEach(async file => {
+    const filePath = path.join(stylesPath, file);
+    const fileContent = await fs.readFile(filePath, 'utf-8');
+    await fs.appendFile(bundlePath, fileContent + '\n', 'utf-8');
+  });
+    
+    console.log('Файл успешно собран');
+    
+}
+
+//program start
+packStyles();
